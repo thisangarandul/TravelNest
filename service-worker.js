a2b075d609e16c56d77693b009419dec194facbb
thisangarandul <thisangarandul445@gmail.com>
2026-05-14 19:41:54 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'travelnest-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/explorer.html',
+  '/planner.html',
+  '/generator.html',
+  '/mood.html',
+  '/feedback.html',
+  '/assets/css/styles.css',
+  '/assets/js/script.js',
+  '/manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate Event (Cleanup old caches)
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
